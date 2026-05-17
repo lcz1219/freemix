41767dd1d457539695ae7506ac0576bc75e39dce
linlinchengzhong <1033519224@qq.com>
2026-05-18 00:53:06 +0800

    tab navui 优化

--- server/src/main/java/com/freemix/freemix/service/AIMessageService.java
+++ server/src/main/java/com/freemix/freemix/service/AIMessageService.java
@@ -100,7 +100,7 @@ public class AIMessageService {
     }
 
     /**
-     * 获取指定会话的消息记录009
+     * 获取指定会话的消息记录0092
      */
     public ApiResponse<List<AIMessage>> getSessionMessages(String sessionId) {
         Query query = new Query(Criteria.where("sessionId").is(sessionId))
